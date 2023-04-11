8e70cf989d925d93912141f17899c0942a0e4265
Jannis Maier <jannismaier97@gmail.com>
2023-04-11 14:28:38 +0200

    asdf

---
+++ exercise_3/target/debug/deps/exercise_3-5aae6a6bcbd0b302.d
@@ -0,0 +1,5 @@
+/home/maierjan/Documents/RustSeminar/exercise_3/target/debug/deps/exercise_3-5aae6a6bcbd0b302.rmeta: src/main.rs
+
+/home/maierjan/Documents/RustSeminar/exercise_3/target/debug/deps/exercise_3-5aae6a6bcbd0b302.d: src/main.rs
+
+src/main.rs:
---
+++ exercise_3/target/debug/deps/exercise_3-76288666953e4e36.d
@@ -0,0 +1,5 @@
+/home/maierjan/Documents/RustSeminar/exercise_3/target/debug/deps/exercise_3-76288666953e4e36.rmeta: src/main.rs
+
+/home/maierjan/Documents/RustSeminar/exercise_3/target/debug/deps/exercise_3-76288666953e4e36.d: src/main.rs
+
+src/main.rs:
